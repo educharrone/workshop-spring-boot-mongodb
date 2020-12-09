2a6a74d21c18d1ad918fb07da62b7f50ccea1c3b
Edu <educharrone@hotmail.com>
2020-12-09 19:50:06 -0300

    Atualização de usuario com PUT

--- src/main/java/com/edu/workshopmongo/resources/UserResource.java
+++ src/main/java/com/edu/workshopmongo/resources/UserResource.java
@@ -64,4 +64,16 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	
 	}
+	
+	@RequestMapping(value="{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> upadate(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj=service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	
+	
+	}
+	
 }
